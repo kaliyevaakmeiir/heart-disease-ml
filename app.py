565a96013b7a484f9224c29959289bf9162d676d
kaliyevaakmeiir <kaliyeva.akmeiir@gmail.com>
2026-01-23 03:13:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ sex = st.selectbox("Sex (0 = female, 1 = male)", [0, 1])
 cp = st.number_input("Chest pain type", 0, 3, 1)
 trestbps = st.number_input("Resting blood pressure", 80, 200, 120)
 chol = st.number_input("Cholesterol", 100, 400, 200)
-fbs = st.selectbox("Fasting blood sugar > 120 mg/dl",
+fbs = st.selectbox("Fasting blood sugar > 120 mg/dl", [0, 1])
 
                    
+
